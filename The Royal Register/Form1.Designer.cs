f1bbce601deb8da2148614c744fff78908b635e3
LavNa <LavNa@LAVNPC012>
2025-03-20 18:50:08 +0200

    Add project files.

---
+++ The Royal Register/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace The_Royal_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtGuestName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.dataGridViewGuests = new System.Windows.Forms.DataGridView();
+            this.txtGuestId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnExportPdf = new System.Windows.Forms.Button();
+            this.txtContactNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGuests)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtGuestName
+            // 
+            this.txtGuestName.Location = new System.Drawing.Point(295, 153);
+            this.txtGuestName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtGuestName.Name = "txtGuestName";
+            this.txtGuestName.Size = new System.Drawing.Size(393, 22);
+            this.txtGuestName.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Guest Name";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.Green;
+            this.btnCheck.Location = new System.Drawing.Point(795, 296);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(156, 44);
+            this.btnCheck.TabIndex = 8;
+            this.btnCheck.Text = "Check / Add Guest";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblResult.Location = new System.Drawing.Point(477, 474);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(21, 20);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "\"\"";
+            // 
+            // dataGridViewGuests
+            // 
+            this.dataGridViewGuests.ColumnHeadersHeight = 29;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Gold;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewGuests.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridViewGuests.Location = new System.Drawing.Point(295, 296);
+            this.dataGridViewGuests.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridViewGuests.Name = "dataGridViewGuests";
+            this.dataGridViewGuests.RowHeadersWidth = 51;
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            this.dataGridViewGuests.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridViewGuests.Size = new System.Drawing.Size(393, 154);
+            this.dataGridViewGuests.TabIndex = 7;
+            // 
+            // txtGuestId
+            // 
+            this.txtGuestId.ForeColor = System.Drawing.Color.Black;
+            this.txtGuestId.Location = new System.Drawing.Point(295, 196);
+            this.txtGuestId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtGuestId.Name = "txtGuestId";
+            this.txtGuestId.Size = new System.Drawing.Size(393, 22);
+            this.txtGuestId.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Guest ID Number";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(438, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.Red;
+            this.btnRemove.Location = new System.Drawing.Point(795, 358);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(156, 44);
+            this.btnRemove.TabIndex = 11;
+            this.btnRemove.Text = "Remove Guest";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnExportPdf
+            // 
+            this.btnExportPdf.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnExportPdf.Location = new System.Drawing.Point(103, 332);
+            this.btnExportPdf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnExportPdf.Name = "btnExportPdf";
+            this.btnExportPdf.Size = new System.Drawing.Size(156, 44);
+            this.btnExportPdf.TabIndex = 12;
+            this.btnExportPdf.Text = "Export Guest List";
+            this.btnExportPdf.UseVisualStyleBackColor = false;
+            this.btnExportPdf.Click += new System.EventHandler(this.btnExportPdf_Click);
+            // 
+            // txtContactNumber
+            // 
+            this.txtContactNumber.Location = new System.Drawing.Point(295, 242);
+            this.txtContactNumber.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtContactNumber.Name = "txtContactNumber";
+            this.txtContactNumber.Size = new System.Drawing.Size(393, 22);
+            this.txtContactNumber.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Guest Contact Number";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Gray;
+            this.btnReset.Location = new System.Drawing.Point(795, 174);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(156, 44);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(1089, 540);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtContactNumber);
+            this.Controls.Add(this.btnExportPdf);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGuestId);
+            this.Controls.Add(this.dataGridViewGuests);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGuestName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "The Royal Register - Guest List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGuests)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuestName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.DataGridView dataGridViewGuests;
+        private System.Windows.Forms.TextBox txtGuestId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnExportPdf;
+        private System.Windows.Forms.TextBox txtContactNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ The Royal Register/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Drawing;
+using System.Xml.Linq;
+using PdfFont = iTextSharp.text.Font;
+using DrawingFont = System.Drawing.Font;
+using PdfImage = iTextSharp.text.Image;
+
+namespace The_Royal_Register
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Server=localhost\SQLEXPRESS03;Database=RoyalRegister;Trusted_Connection=True;Encrypt=False";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadGuestList();
+        }
+
+        // Check/Add Guest Function and Validation
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string guestName = txtGuestName.Text.Trim();
+            string guestIdNumber = txtGuestId.Text.Trim();
+            string contactNumber = txtContactNumber.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(guestName) || string.IsNullOrWhiteSpace(guestIdNumber) || string.IsNullOrWhiteSpace(contactNumber))
+            {
+                MessageBox.Show("Please enter all required fields (Guest Name, ID, and Contact Number).");
+                return;
+            }
+
+            // Validate Contact Number (Allow numbers starting with 0)
+            if (!System.Text.RegularExpressions.Regex.IsMatch(contactNumber, @"^\d+$"))
+            {
+                MessageBox.Show("Please enter a valid contact number.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Check if the guest already exists by IdentityNumber
+                string query = "SELECT COUNT(*) FROM Guests WHERE IdentityNumber = @IdentityNumber";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@IdentityNumber", guestIdNumber);
+                    int count = (int)cmd.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        lblResult.Text = "A guest with this ID already exists in The Royal Register!";
+                    }
+                    else
+                    {
+                        lblResult.Text = "The guest has been added to The Royal Register!";
+                        AddGuestToDatabase(guestName, guestIdNumber, contactNumber);
+                    }
+                }
+            }
+        }
+
+        // Method to Add Guest (Prevents ID duplication)
+        private void AddGuestToDatabase(string guestName, string guestIdNumber, string contactNumber)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Ensure that the textbox data is inserted into the [dbo].[Guests] table on SSMS
+                string query = "INSERT INTO Guests (IdentityNumber, FullName, ContactNumber) VALUES (@IdentityNumber, @FullName, @ContactNumber)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@IdentityNumber", guestIdNumber);
+                    cmd.Parameters.AddWithValue("@FullName", guestName);
+                    cmd.Parameters.AddWithValue("@ContactNumber", contactNumber);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            LoadGuestList();
+        }
+
+        // Button to Remove Guest by ID
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string guestIdNumber = txtGuestId.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(guestIdNumber))
+            {
+                MessageBox.Show("Please enter an ID number to remove a guest.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Check if guest exists before attempting deletion
+                string checkQuery = "SELECT COUNT(*) FROM Guests WHERE IdentityNumber = @IdentityNumber";
+                using (SqlCommand checkCmd = new SqlCommand(checkQuery, conn))
+                {
+                    checkCmd.Parameters.AddWithValue("@IdentityNumber", guestIdNumber);
+                    int count = (int)checkCmd.ExecuteScalar();
+
+                    if (count == 0)
+                    {
+                        MessageBox.Show("No guest found with this ID.");
+                        return;
+                    }
+                }
+
+                // Delete the guest
+                string deleteQuery = "DELETE FROM Guests WHERE IdentityNumber = @IdentityNumber";
+                using (SqlCommand deleteCmd = new SqlCommand(deleteQuery, conn))
+                {
+                    deleteCmd.Parameters.AddWithValue("@IdentityNumber", guestIdNumber);
+                    int rowsAffected = deleteCmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Guest has been removed from The Royal Register.");
+                        LoadGuestList(); // Refresh the guest list
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error: Guest could not be removed.");
+                    }
+                }
+            }
+        }
+
+        // Load the Guest List into DataGridView
+        private void LoadGuestList()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT IdentityNumber, FullName, ContactNumber FROM Guests ORDER BY UserID ASC";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dataGridViewGuests.DataSource = dt;
+                }
+            }
+        }
+
+        // Export Guest List to PDF
+        private void ExportGuestListToPdf(string filePath)
+        {
+            Document doc = new Document();
+            try
+            {
+                PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+                doc.Open();
+
+                // Add logo image to PDF
+                string logoPath = @"/Users/LavNa/Downloads/RoyalReg.jpg";  // Should work on macOS or Linux                                                                           // Replace with the actual path to your logo
+                if (File.Exists(logoPath))
+                {
+                    PdfImage logo = PdfImage.GetInstance(logoPath);
+                    logo.ScaleToFit(100f, 100f); // Resize logo (optional)
+                    logo.Alignment = PdfImage.ALIGN_CENTER;  // Center the logo
+                    doc.Add(logo); // Add the logo to the document
+                }
+
+                // Using PdfFont
+                PdfFont titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 18, BaseColor.BLACK);
+                Paragraph title = new Paragraph("The Royal Register - Guest List\n\n", titleFont);
+                title.Alignment = Element.ALIGN_CENTER;
+                doc.Add(title);
+
+                // Table Setup
+                PdfPTable table = new PdfPTable(3);
+                table.WidthPercentage = 100;
+                //table.SetWidths(new float[] {1, 2, 3});
+                table.SetWidths(new float[] { 2, 3, 3 });
+
+                // Add Headers
+                PdfFont headerFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12, BaseColor.WHITE);
+                BaseColor headerColor = new BaseColor(65, 105, 225); // Royal Blue
+                PdfPCell cell;
+
+                cell = new PdfPCell(new Phrase("ID Number", headerFont)) { BackgroundColor = headerColor };
+                table.AddCell(cell);
+                cell = new PdfPCell(new Phrase("Full Name", headerFont)) { BackgroundColor = headerColor };
+                table.AddCell(cell);
+                cell = new PdfPCell(new Phrase("Contact Number", headerFont)) { BackgroundColor = headerColor };
+                table.AddCell(cell);
+
+                // Fetch Guest Data
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT IdentityNumber, FullName, ContactNumber FROM Guests ORDER BY UserID ASC";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //table.AddCell(reader["UserID"].ToString());
+                                //table.AddCell(reader["IdentityNumber"].ToString());
+                                //table.AddCell(reader["FullName"].ToString());
+                                //table.AddCell(reader["ContactNumber"].ToString());
+
+                                //Replaced code above due to error handling for NULL values
+
+                                table.AddCell(reader["IdentityNumber"] != DBNull.Value ? reader["IdentityNumber"].ToString() : "N/A");
+                                table.AddCell(reader["FullName"] != DBNull.Value ? reader["FullName"].ToString() : "N/A");
+                                table.AddCell(reader["ContactNumber"] != DBNull.Value ? reader["ContactNumber"].ToString() : "N/A");
+
+                            }
+                        }
+                    }
+                }
+
+                //Table Added
+                doc.Add(table);
+                doc.Close();
+
+                MessageBox.Show("Guest list exported successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error exporting PDF: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Export Button Functionality
+        private void btnExportPdf_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "PDF Files|*.pdf";
+            saveDialog.Title = "Save Guest List as PDF";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportGuestListToPdf(saveDialog.FileName);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtGuestName.Text = "";
+            txtGuestId.Text = "";
+            txtContactNumber.Text = "";
+        }
+    }
+}
